56fd8689428a9fe047915ef8a3ae7e0cdbc6309e
Jorge Lopez <JorgeLopez5252@gmail.com>
2019-11-19 22:15:56 -0700

    updated with functions to my choices, need to set parameters

---
+++ db/seed.sql
@@ -0,0 +1,16 @@
+USE employeetracker_db;
+
+
+
+
+
+INSERT INTO department (id, name,)
+VALUES (1, "deptName");
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES (1, "title", INT, INT);
+​
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (1,"first_name", "last_name", INT, INT);
+
+
--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const { prompt } = require("inquirer");
 require("console.table");
 const db = require("./db");
 
-
 init();
 function init() {
   CallPrompt();
@@ -24,25 +23,80 @@ async function CallPrompt() {
           value: "view_employees_by_department"
         },
         {
-          name: "View All Employees by Mangaer",
+          name: "View All Employees by Manager",
           value: "view_employees_by_manager"
+        },
+        {
+          name: "Add Employee",
+          value: "add_employee"
+        },
+        {
+          name: "Delete Employee",
+          value: "delete_employee"
+        },
+        {
+          name: "Update Employee role",
+          value: "update_employee_role"
+        },
+        {
+          name: "Update Employee Manager",
+          value: "update_employee_manager"
+        },
+        {
+          name: "View All Roles",
+          value: "view_all_roles"
         }
       ]
     }
   ]);
-  switch(choice){
-      case "view_employees": return viewEmployees();
-      case "view_employees_by_department": return viewEmployeesByDepartment();
-      case "view_employees_by_manager": return viewEmployeesByManager();
-      default: console.log("No options");
+  switch (choice) {
+    case "view_employees":
+      return viewEmployees();
+    case "view_employees_by_department":
+      return viewEmployeesByDepartment();
+    case "view_employees_by_manager":
+      return viewEmployeesByManager();
+    case "add_employee":
+      return addEmployee();
+    case "delete_employee":
+      return deleteEmployee();
+    case "update_employee_role":
+      return updateEmployee();
+    case "update_employee_manager":
+      return updateEmployee();
+    case "view_all_roles":
+      return viewAllRoles();
+    default:
+      console.log("No options");
   }
 }
 
-async function viewEmployees(){
+async function viewEmployees() {
+  const employees = await db.findAllEmployees();
+  console.table(employees);
+  CallPrompt();
+}
 
-    const employees  = await db.findAllEmployees();
-    console.table(employees);
-    CallPrompt();
+async function viewEmployeesByDepartment() {
 
+}
+
+async function viewEmployeesByManager() {
+
+}
+
+async function addEmployee() {
+
+}
+
+async function deleteEmployee() {
+
+}
+
+async function updateEmployee() {
+
+}
+
+async function viewAllRoles() {
 
 }
